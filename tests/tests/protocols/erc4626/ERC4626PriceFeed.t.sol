1c86bb444d37461ecf77b062b75bc420dbc7b5b6
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-07-07 20:46:01 +0200

    test: improve erc4626 price feed (#1252)

--- tests/tests/protocols/erc4626/ERC4626PriceFeed.t.sol
+++ tests/tests/protocols/erc4626/ERC4626PriceFeed.t.sol
@@ -2,9 +2,17 @@
 pragma solidity 0.8.19;
 
 import {IERC4626} from "openzeppelin-solc-0.8/token/ERC20/extensions/ERC4626.sol";
+
+import {IChainlinkPriceFeedMixin as IChainlinkPriceFeedMixinProd} from
+    "contracts/release/infrastructure/price-feeds/primitives/IChainlinkPriceFeedMixin.sol";
+
 import {IntegrationTest} from "tests/bases/IntegrationTest.sol";
+
 import {IERC20} from "tests/interfaces/external/IERC20.sol";
+
 import {IERC4626PriceFeed} from "tests/interfaces/internal/IERC4626PriceFeed.sol";
+import {IValueInterpreter} from "tests/interfaces/internal/IValueInterpreter.sol";
+
 import {
     ETHEREUM_MORPHO_MAWETH_VAULT_ADDRESS,
     ETHEREUM_MORPHO_MA3WETH_VAULT_ADDRESS,
@@ -13,14 +21,16 @@ import {
 } from "./ERC4626Utils.sol";
 
 abstract contract ERC4626PriceFeedTestBase is IntegrationTest {
-    IERC4626PriceFeed internal erc4626PriceFeed;
+    IERC4626PriceFeed internal priceFeed;
     IERC4626 internal erc4626Vault;
     IERC20 internal underlying;
 
-    function setUp(address _erc4626VaultAddress) internal {
-        erc4626PriceFeed = __deployPriceFeed();
-        erc4626Vault = IERC4626(_erc4626VaultAddress);
-        underlying = IERC20(erc4626Vault.asset());
+    EnzymeVersion internal version;
+
+    function __initialize(EnzymeVersion _version, uint256 _chainId) internal {
+        setUpNetworkEnvironment(_chainId);
+        priceFeed = __deployPriceFeed();
+        version = _version;
     }
 
     // DEPLOYMENT HELPERS
@@ -30,55 +40,98 @@ abstract contract ERC4626PriceFeedTestBase is IntegrationTest {
         return IERC4626PriceFeed(addr);
     }
 
-    function test_calcUnderlyingValues_success() public {
-        uint256 derivativeAmount = assetUnit({_asset: IERC20(address(erc4626Vault))}) * 3;
-        uint256 expectedUnderlyingValue = erc4626Vault.convertToAssets({shares: derivativeAmount});
+    // TESTS
 
-        (address[] memory underlyingAddresses, uint256[] memory underlyingValues) = erc4626PriceFeed
-            .calcUnderlyingValues({_derivative: address(erc4626Vault), _derivativeAmount: derivativeAmount});
+    function __test_calcUnderlyingValues_success(
+        address _erc4626VaultAddress,
+        uint256 _allowedDeviationPer365DaysInBps,
+        uint256 _poolCreationTimestamp
+    ) internal {
+        addDerivative({
+            _valueInterpreter: IValueInterpreter(getValueInterpreterAddressForVersion(version)),
+            _tokenAddress: _erc4626VaultAddress,
+            _skipIfRegistered: false,
+            _priceFeedAddress: address(priceFeed)
+        });
 
-        assertEq(
-            toArray(address(underlying)), underlyingAddresses, "Mismatch between actual and expected underlying address"
-        );
-        assertEq(
-            toArray(expectedUnderlyingValue), underlyingValues, "Mismatch between actual and expected underlying value"
+        address underlyingAddress = IERC4626(_erc4626VaultAddress).asset();
+
+        uint256 erc4626VaultValue = IValueInterpreter(getValueInterpreterAddressForVersion(version))
+            .calcCanonicalAssetValue({
+            _baseAsset: _erc4626VaultAddress,
+            _amount: assetUnit(IERC20(_erc4626VaultAddress)),
+            _quoteAsset: underlyingAddress
+        });
+
+        uint256 underlyingSingleUnit = assetUnit(IERC20(underlyingAddress));
+        uint256 timePassed = block.timestamp - _poolCreationTimestamp;
+
+        assertGe(erc4626VaultValue, underlyingSingleUnit, "Value too low");
+        assertLe(
+            erc4626VaultValue,
+            underlyingSingleUnit
+                + (underlyingSingleUnit * _allowedDeviationPer365DaysInBps * timePassed)
+                    / (365 days * BPS_ONE_HUNDRED_PERCENT),
+            "Deviation to high"
         );
     }
 
-    function test_isSupportedAsset_success() public {
-        assertTrue(erc4626PriceFeed.isSupportedAsset({_asset: address(erc4626Vault)}), "Unsupported erc4626 token");
+    function __test_isSupportedAsset_success(address _erc4626VaultAddress) internal {
+        assertTrue(priceFeed.isSupportedAsset({_asset: _erc4626VaultAddress}), "Unsupported erc4626 token");
     }
 
     function test_isSupportedAsset_failWithoutExpectedInterface() public {
         vm.expectRevert();
-        erc4626PriceFeed.isSupportedAsset({_asset: address(underlying)});
+        priceFeed.isSupportedAsset({_asset: makeAddr("Unsupported Address")});
     }
 }
 
-contract MorphoAaveV2Test is ERC4626PriceFeedTestBase {
-    function setUp() public override {
-        setUpMainnetEnvironment();
-        super.setUp(ETHEREUM_MORPHO_MAWETH_VAULT_ADDRESS);
+abstract contract ERC4626PriceFeedTestEthereumBase is ERC4626PriceFeedTestBase {
+    function __initialize(EnzymeVersion _version) internal {
+        __initialize({_version: _version, _chainId: ETHEREUM_CHAIN_ID});
+    }
+
+    function test_calcUnderlyingValuesMorphoAaveV2_success() public {
+        __test_calcUnderlyingValues_success({
+            _erc4626VaultAddress: ETHEREUM_MORPHO_MAWETH_VAULT_ADDRESS,
+            _allowedDeviationPer365DaysInBps: 3 * BPS_ONE_PERCENT,
+            _poolCreationTimestamp: 1665076991
+        });
+    }
+
+    function test_calcUnderlyingValuesMorphoAaveV3_success() public {
+        __test_calcUnderlyingValues_success({
+            _erc4626VaultAddress: ETHEREUM_MORPHO_MA3WETH_VAULT_ADDRESS,
+            _allowedDeviationPer365DaysInBps: 3 * BPS_ONE_PERCENT,
+            _poolCreationTimestamp: 1686751511
+        });
+    }
+
+    function test_calcUnderlyingValuesMorphoCompound_success() public {
+        __test_calcUnderlyingValues_success({
+            _erc4626VaultAddress: ETHEREUM_MORPHO_MCWETH_VAULT_ADDRESS,
+            _allowedDeviationPer365DaysInBps: BPS_ONE_PERCENT,
+            _poolCreationTimestamp: 1665585731
+        });
+    }
+
+    function test_calcUnderlyingValuesSpark_success() public {
+        __test_calcUnderlyingValues_success({
+            _erc4626VaultAddress: ETHEREUM_SPARK_SDAI_VAULT_ADDRESS,
+            _allowedDeviationPer365DaysInBps: 7 * BPS_ONE_PERCENT,
+            _poolCreationTimestamp: 1673977931
+        });
     }
 }
 
-contract MorphoAaveV3Test is ERC4626PriceFeedTestBase {
+contract ERC4626PriceFeedTestEthereum is ERC4626PriceFeedTestEthereumBase {
     function setUp() public override {
-        setUpMainnetEnvironment();
-        super.setUp(ETHEREUM_MORPHO_MA3WETH_VAULT_ADDRESS);
+        __initialize(EnzymeVersion.Current);
     }
 }
 
-contract MorphoCompoundTest is ERC4626PriceFeedTestBase {
+contract ERC4626PriceFeedTestEthereumV4 is ERC4626PriceFeedTestEthereumBase {
     function setUp() public override {
-        setUpMainnetEnvironment();
-        super.setUp(ETHEREUM_MORPHO_MCWETH_VAULT_ADDRESS);
-    }
-}
-
-contract SparkTest is ERC4626PriceFeedTestBase {
-    function setUp() public override {
-        setUpMainnetEnvironment();
-        super.setUp(ETHEREUM_SPARK_SDAI_VAULT_ADDRESS);
+        __initialize(EnzymeVersion.V4);
     }
 }
